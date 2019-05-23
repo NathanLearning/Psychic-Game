9074bcaab31adf8c7ecd64461a4a3b2105a990d8
Nathan Visti <nathanvisti@gmail.com>
2019-05-23 18:01:00 -0400

    Fixed computer selecting new letter every keystroke... Recording guesses left and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,26 @@ var stats = {
     losses: 0,
     guessesLeft: 10
 }
+var compSelect = compChoices[Math.floor(Math.random() * compChoices.length)]
 
 document.onkeyup = function(event){
     var userGuess = event.key
-    var compSelect = compChoices[Math.floor(Math.random() * compChoices.length)]
     
     if (userGuess === compSelect) {
         stats.wins = stats.wins + 1
         document.getElementById("winsCount").textContent ="Number of wins: " + stats.wins
     }
+
+    if (userGuess != compSelect) { 
+        stats.guessesLeft = stats.guessesLeft + -1
+        document.getElementById("guessRem").textContent ="Guesses remaining: " + stats.guessesLeft
+    }
+
+    if (stats.guessesLeft < 1) {
+        stats.losses = stats.losses + 1
+        document.getElementById("lossesCount").textContent ="Number of losses: " + stats.losses
+       
+    }
+
+
 }
