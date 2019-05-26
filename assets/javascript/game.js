aea65addf6d895448fd8a27606d21430c1aae473
Nathan Visti <nathanvisti@gmail.com>
2019-05-26 13:07:57 -0400

    added empty array, guessedLetters.
    Have major bug winning or lossing doesnt change computer letter choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,40 @@ console.log(compChoices)
 var stats = {
     wins: 0,
     losses: 0,
-    guessesLeft: 10
+    guessesLeft: 10,
+    guessedLetters: []
 }
 var compSelect = compChoices[Math.floor(Math.random() * compChoices.length)]
 
-document.onkeyup = function(event){
+document.onkeyup = function(event) {
     var userGuess = event.key
     
     if (userGuess === compSelect) {
-        stats.wins = stats.wins + 1
-        document.getElementById("winsCount").textContent ="Number of wins: " + stats.wins
+        stats.wins++
+        stats.guessesLeft = 10
+        stats.guessedLetters = []
     }
 
-    if (userGuess != compSelect) { 
-        stats.guessesLeft = stats.guessesLeft + -1
-        document.getElementById("guessRem").textContent ="Guesses remaining: " + stats.guessesLeft
+    if (userGuess !== compSelect) { 
+        stats.guessesLeft--
     }
 
-    if (stats.guessesLeft < 1) {
-        stats.losses = stats.losses + 1
-        document.getElementById("lossesCount").textContent ="Number of losses: " + stats.losses
-       
+    if (stats.guessesLeft == 0) {
+        stats.losses++
+        stats.guessedLetters = []
+        stats.guessesLeft = 10
+    }
+
+    if  (stats.guessedLetters.indexOf(userGuess) >= 0) {
+    
+    } else {
+        stats.guessedLetters.push(userGuess)
+        document.getElementById("guessLet").innerHTML = stats.guessedLetters 
+        console.log(stats.guessedLetters)
     }
 
 
+    document.getElementById("winsCount").textContent = "Number of wins: " + stats.wins
+    document.getElementById("guessRem").textContent = "Guesses remaining: " + stats.guessesLeft
+    document.getElementById("lossesCount").textContent = "Number of losses: " + stats.losses
 }
